bbea9fa045400a7e0aae9b076e574830d425cdae
Elijah Bobzom <ebobzom@gmail.com>
2020-08-07 13:33:26 +0100

    modified confirm email to always redirect

--- src/controllers/GET/confirmEmail.js
+++ src/controllers/GET/confirmEmail.js
@@ -10,21 +10,16 @@ confirmEmailController.get('/:userId', (req, res) => {
      db.query(confirmEmailQuery,(err, result) => {
         if(err){
             logError(err);
-            res.status(500).json({
-                status: 'error',
-                msg: "please click confirm email link again"
-            });
-            return;
+            // TODO: redirect to error page, with message click link again
+            return res.redirect('https://google.com');
         }
         if(result.affectedRows > 0){
             res.redirect('https://google.com'); //resdirect to login page ( add a params that front end can use to determine if confirmed so a message is shown)
             return;
         }
-        res.status(400).json({
-            status: 'error',
-            msg: "invalid user detail"
-        });
-        return;
+
+        // TODO: redirect to error page, with message click link again
+        return res.redirect('https://google.com');
      });
     
 });
