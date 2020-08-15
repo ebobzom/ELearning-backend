f7603a67e7c7a6b4bbe2a126dc51f7ab83187bfb
Elijah Bobzom <ebobzom@gmail.com>
2020-08-15 18:25:18 +0100

    made logic change in get courses

--- src/controllers/GET/getUsers.js
+++ src/controllers/GET/getUsers.js
@@ -32,10 +32,12 @@ getCourseRouter.get('/courses/:amount', verifyTokenForFetchingCourses, (req, res
         });
     }else{
         let query = `SELECT course_id, course_title, subject, c.description as course_description, course_url, 
-        course_duration, full_name as teacher, t.description teacher_info, paid_course FROM courses AS c, teachers AS t WHERE owner_email='${res.payload.email}' OR paid_course = 0`;
+        course_duration, full_name as teacher, t.description teacher_info FROM courses AS c, teachers AS t WHERE paid_course = 0`;
+        
+        let secondQuery = `SELECT course_id, course_title, subject, c.description as course_description, course_url, 
+        course_duration, full_name as teacher, t.description teacher_info FROM courses AS c, teachers AS t WHERE owner_email='${res.payload.email}'`;
         
         if(typeof Number(amount) === 'number'){
-            console.log('entered')
             query += ` LIMIT ${amount}`;
         }
         
@@ -47,11 +49,22 @@ getCourseRouter.get('/courses/:amount', verifyTokenForFetchingCourses, (req, res
                     msg: 'an error occured, cross check your parameters'
                 });
             }
+            db.query(secondQuery, (err1, result2) => {              
+                if(err1){
+                    logError(err1);
+                    return res.status(400).json({
+                        status: 'error',
+                        msg: 'an error occured, cross check your parameters'
+                    });
+                }
 
-        
-            return res.status(200).json({
-                status: 'success',
-                data: result
+                return res.status(200).json({
+                    status: 'success',
+                    data: {
+                        unpaidCourses: result,
+                        paidCourses: result2
+                    }
+                });
             });
         });
     }
