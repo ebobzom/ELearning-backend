ecdd039dc8db669cff4d23f02c79b00107897a04
Elijah Bobzom <ebobzom@gmail.com>
2020-08-17 09:00:35 +0100

    addeded signup validation

--- src/controllers/CREATE/signup.js
+++ src/controllers/CREATE/signup.js
@@ -6,12 +6,20 @@ const { validationResult } = require('express-validator');
 const sendEmail = require('../../utils/sendEmail');
 const db = require('../../config/db');
 const logError = require('../../utils/logErrors');
+const { check } = require('express-validator');
 
 const signuRouter = express.Router();
 
-// const signupValidation = require('./../../validation/CREATE/signup-validation');
+const signupValidation = [
+    check('firstName').isString(),
+    check('firstName').isLength({ max: 20 }).withMessage('first name must be less then 20 characters'),
+    check('lastName').isString(),
+    check('lastName').isLength({ max: 20 }).withMessage('last name must be less then 20 characters'),
+    check('email').isEmail().withMessage('invalid email format'),
+    check('password').isLength({ min: 5 }).withMessage('password must be more than 5 and less than 16 characters')
+]
 
-signuRouter.post('/', (req, res) => {
+signuRouter.post('/', signupValidation, (req, res) => {
 
     const errors = validationResult(req);
     if(!errors.isEmpty()){
